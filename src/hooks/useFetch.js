2f51fe290ac2b8b058de8738d2b6ed30e9e6579a
Cian Butler <cianbtlr3@gmail.com>
2023-03-28 23:32:50 +0100

    Backend url

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const useFetch = (url) => {
+const useFetch = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
@@ -10,7 +10,7 @@ const useFetch = (url) => {
     const fetchData = async () => {
       setLoading(true);
       try {
-        const res = await axios.get(url);
+        const res = await axios.get("https://irishairways.adaptable.app/api");
         setData(res.data);
       } catch (err) {
         setError(err);
@@ -18,12 +18,12 @@ const useFetch = (url) => {
       setLoading(false);
     };
     fetchData();
-  }, [url]);
+  }, []);
 
   const reFetch = async () => {
     setLoading(true);
     try {
-      const res = await axios.get(url);
+      const res = await axios.get("https://irishairways.adaptable.app/api");
       setData(res.data);
     } catch (err) {
       setError(err);
