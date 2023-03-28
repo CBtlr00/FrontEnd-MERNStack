b25b523034f434475a5726cba1f5aba29a927e23
Cian Butler <cianbtlr3@gmail.com>
2023-03-29 00:14:32 +0100

    Backend url fix

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -18,7 +18,7 @@ const useFetch = (url) => {
       setLoading(false);
     };
     fetchData();
-  }, ["https://irishairways.adaptable.app/api"+url]);
+  }, []);
 
   const reFetch = async () => {
     setLoading(true);
